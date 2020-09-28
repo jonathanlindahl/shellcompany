54e330ee31b00b7b98a20d36fa7de2325369b7e7
Jonathan Lindahl <jonathanlindahl3@gmail.com>
2020-09-28 09:24:42 +0200

    first commit

---
+++ src/main/java/com/domain/shellcompany/ShellcompanyApplication.java
@@ -0,0 +1,13 @@
+package com.domain.shellcompany;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShellcompanyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ShellcompanyApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/domain/shellcompany/ShellcompanyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.domain.shellcompany;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShellcompanyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
